0074fc20777fc5eaddb5f4f3ee8b0c7a9150eef7
john mangual <john.mangual@gmail.com>
2015-09-13 13:30:23 -0400

    Update square-1.elm

--- square-1.elm
+++ square-1.elm
@@ -9,25 +9,23 @@ main : Element
 main =
   collage 250 450
     [ move (-55,  -55 )   blueSquare
-    , move (-55,   55 )    redSquare
-    , move ( 55,  -55 )  greenSquare
-    , move ( 55, 3*55 ) yellowSquare
+    , rotate (degrees -90)  ( move (-55,   55 )    redSquare )
+    , rotate (degrees  90)  ( move ( 55,  -55 )  greenSquare )
+    , move ( 55,  55 ) yellowSquare
     ]
     
-
-
+-- 
+varDashed x y = LineStyle x 2 Flat Smooth y 10
 
 -- traced : LineStyle -> Path -> Form
 
 blueSquare : Form
 blueSquare =
-  traced (dashed blue) square
-
-
+  traced (varDashed blue [8,4,4,4] ) square
 
 redSquare : Form
 redSquare  =
-  traced ( solid red) square
+  traced (solid red) square
   
 greenSquare : Form
 greenSquare =
@@ -35,10 +33,10 @@ greenSquare =
   
 yellowSquare : Form
 yellowSquare = 
-  traced (dotted yellow) square
+  traced (varDashed yellow [8,4] ) square
 
 -- path : List ( Float, Float ) -> Path
 
 square : Path
 square =
-  path [ (50,50), (50,-50), (-50,-50), (-50,50), (50,50) ]
+  path [ (0,50), (0,0), (50,0), (50,-50), (-50,-50), (-50,50),  (0,50) ]
