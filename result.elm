8d7e10e773fc2eca45d914b2504b9442b17490ae
john mangual <john.mangual@gmail.com>
2016-01-23 21:17:12 -0400

    example where in put fails

--- result.elm
+++ result.elm
@@ -1,28 +1,18 @@
 import Html exposing (text)
 import String
 
-{- 
-> f x = x + 1
-<function> : number -> number
-> g x = (Result.map f ) x
-<function> : Result.Result a number -> Result.Result a number
-> import String
-> g(String.toInt "5")
-Ok 6 : Result.Result String Int
-> toString (g(String.toInt "5"))
-"Ok 6" : String
-> (Result.map toString )(g(String.toInt "5"))
-Ok "6" : Result.Result String String
-> import Html exposing (text)
-> text "5"
-{ text = "5", version = "2", type = "VirtualText" } : Html.Html
--}
-
 f: Int -> Int
 f x = x + 1
 
 g: Result String Int -> Result String Int
 g x = (Result.map f) x
 
+
+h x = 
+    case x of 
+        Ok value -> toString(value + 1)
+        Err msg  -> "Try a different value"
+
+
 main =
-  text (  toString ( g  (String.toInt "5" ) ))
+  text (  toString ( h (g  (String.toInt "x" ) )))
