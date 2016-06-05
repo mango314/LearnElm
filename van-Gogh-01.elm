08903dd09526f08a468730d7fbc31920824e289f
john mangual <john.mangual@gmail.com>
2016-06-04 17:46:02 -0800

    introduce a mistake on line 40 (it's still there)

--- van-Gogh-01.elm
+++ van-Gogh-01.elm
@@ -1,26 +1,91 @@
-import Html exposing (Html)
+port module VanGogh exposing (..)
 
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.App as Html
+-- https://developer.mozilla.org/en-US/docs/Web/API/HTMLImageElement/Image
 import Element exposing (image, toHtml)
+-- import Random  exposing (..)
+import Collage exposing (..)
 
-main = toHtml ( image 960 500 "starry-night.jpg" )
+import String
+import List
+import Task
+import Color exposing (..)
+
+width  = 960
+height = 500
+
+--main = toHtml ( image width height "starry-night.jpg" )
+
+main =
+  Html.program
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    }
+
+-- subscriptions
+
+port getImageData : () -> Cmd msg
+
+port getPolygons : () -> Cmd msg
+
+port polygons : ( ( List Polygon ) -> msg ) -> Sub msg
+
+port pixels : ( ImageData -> msg ) -> Sub msg
+
+subscriptions: Model -> Sub Msg
+subscriptions model = pixels ImageData
+
+-- MODEL
+-- type alias Model = List (Int, Int)
+
+type alias Point = (Float, Float)
+
+type alias Polygon = List Point
+
+type alias ImageData = {width: Int, height: Int, pts: List Int}
+
+type alias Model = { polygons : List Polygon, pic: ImageData }
 
 
-{-
+-- VIEW
 
-three main functions in mbstock's Starry Night Example
+view: Model -> Html Msg
+view model = div [ class "title"]
+  [ h1 [] [ Html.text "Voronoi Tesslation Collage" ]
+  , h2 [] [ Html.text "Generating New Results!" ]
+  , toHtml <| image width height "starry-night.jpg"
+  , toHtml <| collage width height <| tiling model.polygons
+  ]
 
-* best candidate sampler
+tiling: List Polygon -> List Form
+tiling x = List.map tile x
 
- --- implement QUADTREE in Elm instead of messy Javascript
+tile:  Polygon -> Form
+tile y = ( outlined <| solid black ) (polygon y)
 
-* d3.geom.voronoi
+-- UPDATE
 
- --- borrow from d3.js using Elm's interOp
+type Msg = GetData | MyPolygons (List Polygon) | GetPolygon | MyImage ImageData
 
- * image data ---> voronoi( best candidate samples) -->  fill canvas with polygons 
 
- --- https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/getImageData
+update: Msg -> Model -> (Model, Cmd msg)
+update msg model =
+  case msg of
+    GetData    -> (model   , getImageData () )
+    GetPolygon -> (model   , getPolygons  () )
+    MyPolygons x -> (Model x model.pic      , Cmd.none        )
+    MyImage  x   -> (Model model.polygons x , Cmd.none        )
 
- --- also requires interOP
 
--}
+-- INIT
+
+init: (Model, Cmd Msg)
+init = ( Model [] <| ImageData width height [] , send GetPolygon )
+
+send : msg -> Cmd msg
+send msg =
+  Task.perform identity identity (Task.succeed msg)
