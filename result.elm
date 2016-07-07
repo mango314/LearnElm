345a750c4dc9bebb057f2f6fa18ecebaef35373f
JDM <john.mangual@gmail.com>
2016-07-07 17:47:10 -0400

    example I got stuck on a long time ago for StackOverflow

--- result.elm
+++ result.elm
@@ -7,12 +7,10 @@ f x = x + 1
 g: Result String Int -> Result String Int
 g x = (Result.map f) x
 
-
-h x = 
-    case x of 
+h x =
+    case x of
         Ok value -> toString(value + 1)
         Err msg  -> "Try a different value"
-
 
 main =
   text (  toString ( h (g  (String.toInt "x" ) )))
