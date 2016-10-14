1a1e75bc64f319dfea002ac95626eb722d9781a8
john mangual <john.mangual@gmail.com>
2016-10-14 08:43:56 -0800

    independent from the previous example

--- button.elm
+++ button.elm
@@ -1,12 +1,32 @@
-import Html exposing (Html, button, div, text, span)
-import Html.App as App
+import Html exposing (div, span, button, text, Attribute)
+import Html.App exposing (beginnerProgram)
 import Html.Events exposing (onClick)
-import Html.Attributes as Attribute exposing ( style)
+import Html.Attributes exposing (style)
+
 
 main =
-  App.beginnerProgram { model = 0, view = view, update = update }
+  beginnerProgram { model = 0, view = view, update = update }
+
+-- Elm does not save us from CSS it only makes it more manageable
+-- https://developer.mozilla.org/en-US/docs/Web/CSS/display
+-- https://developer.mozilla.org/en-US/docs/Web/CSS/float
+
+buttonStyle : Attribute msg
+buttonStyle = style [ ("width", "25px"),("float", "left") ]
+
+numStyle : Attribute msg
+numStyle = style [ ("width", "25px"), ("float", "left"), ("text-align", "center")]
+
+view model =
+  div []
+    [ button [ onClick Decrement, buttonStyle ] [ text "-" ]
+    , span [ numStyle ] [ text (toString model) ]
+    , button [ onClick Increment, buttonStyle ] [ text "+" ]
+    ]
+
 
 type Msg = Increment | Decrement
+
 
 update msg model =
   case msg of
@@ -15,13 +35,3 @@ update msg model =
 
     Decrement ->
       model - 1
-
-view model =
-  div []
-    [ button [ onClick Decrement , myStyle] [ text "-" ]
-    , div [ myStyle ] [ text (toString model) ]
-    , button [ onClick Increment, myStyle ] [ text "+" ]
-    ]
-
-myStyle : Html.Attribute msg
-myStyle = style [ ( "width", "25px"  ), ("background-color", "#FFE1C0"), ("float", "left"), ("text-align", "center")]
