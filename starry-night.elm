3f14b4fc2f2e047237a7e22a2b19f1048f6bd542
john mangual <john.mangual@gmail.com>
2016-10-23 19:32:54 -0400

    presentable... (unfinished)

--- starry-night.elm
+++ starry-night.elm
@@ -3,8 +3,11 @@ port module VanGogh exposing (..)
 import Html exposing (div, span, button, text, Attribute, img, Html)
 import Html.App exposing (program)
 import Html.Events exposing (onClick, onMouseOver, onMouseOut)
-import Html.Attributes exposing (style, src, width)
+import Html.Attributes as Html exposing (style, src, width, height)
 import Mouse exposing (Position)
+
+import Svg exposing (rect, svg)
+import Svg.Attributes as Svg exposing (width, height, viewBox, x, y)
 
 
 main =
@@ -15,7 +18,7 @@ main =
 -- https://developer.mozilla.org/en-US/docs/Web/CSS/float
 
 subscriptions: Model -> Sub Msg
-subscriptions model = Mouse.moves MouseLocation
+subscriptions model = Sub.batch [ Mouse.moves MouseLocation, getImageData GetImage  ]
 
 type alias Model = { n: Int, x: Int, y: Int, p: Bool, img: List Int }
 
@@ -35,23 +38,32 @@ display model =
   else
     text <| "Mouse Location:" ++ " -------- "
 
+-- idiosyncracy
+-- Svg.width  : String -> Attribute Msg
+-- Html.width : Int    -> Attribute Msg
+
+square: Model -> Html Msg
+square model = svg [ Svg.width "20", Svg.height "20", Svg.viewBox "0 0 20 20"] [
+  rect [ Svg.x "0", Svg.y "0", Svg.width "20", Svg.height "20" ] [] ]
 
 view: Model -> Html Msg
 view model =
 
-  div [ width 500 ] [
-    div [ width 500  ]
+  div [ Html.width 500 ] [
+    div [   ]
       [ button [ onClick Decrement, buttonStyle ] [ text "-" ]
       , span [ numStyle ] [ text (toString model.n) ]
       , button [ onClick Increment, buttonStyle ] [ text "+" ]
       , div [ style [ ("width", "200px"), ("text-align", "center"), ("display", "inline-block")] ] [ display model ]
-      , div [ style [ ("width", "200px"), ("text-align", "center"), ("display", "inline-block")] ] [text <| "Mouse Over: " ++ (toString model.p) ]
-      , span [ width 100] [ text "."]
+      , div [ style [ ("width", "125px"), ("text-align", "center"), ("display", "inline-block")] ] [text <| "Mouse Over: " ++ (toString model.p) ]
+--      , div [ style [ ("width", "125px"), ("text-align", "center"), ("display", "inline-block")] ] [text <| "Picture: " ++ (toString model.img) ]
+      , div [ style [ ("width", "125px"), ("text-align", "center"), ("display", "inline-block")] ] [ square model ]
+      , button [onClick CheckImage ] [ text "pixelate"]
       ] ,
-    div [ width 500, style [ ("float", "left")]  ] [img [ src "starry-night.jpg", width 500, onMouseOver ShowLocation, onMouseOut HideLocation] [] ]
+    div [ Html.width 500, style [ ("float", "left")]  ] [img [ src "starry-night.jpg", Html.width 500, Html.height 300,  onMouseOver ShowLocation, onMouseOut HideLocation] [] ]
     ]
 
-type Msg = Increment | Decrement | MouseLocation Position | ShowLocation | HideLocation
+type Msg = Increment | Decrement | MouseLocation Position | ShowLocation | HideLocation | CheckImage | GetImage ( List Int )
 
 --showLocation =
 --  case msg of
@@ -63,15 +75,19 @@ update: Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
   case msg of
     Increment ->
-      ( { x = model.x, y = model.y, n = model.n + 1, p = model.p, img= model.img }, Cmd.none)
+      ( {model | n = model.n + 1 } , Cmd.none)
     Decrement ->
-      ( { x = model.x, y = model.y, n = model.n - 1, p = model.p , img= model.img}, Cmd.none)
+      ( {model | n = model.n - 1 }, Cmd.none)
     MouseLocation pt->
-      ( { x = pt.x, y = pt.y, n = model.n, p = model.p , img= model.img}, Cmd.none )
+      ( {model| x = pt.x, y = pt.y} , Cmd.none )
     ShowLocation ->
-      ( { x = model.x, y = model.y, n = model.n , p = True , img= model.img} , Cmd.none)
+      ( {model | p  = True } , Cmd.none)
     HideLocation ->
-      ( { x = model.x, y = model.y, n = model.n , p = False, img= model.img } , Cmd.none)
+      ( {model | p = False } , Cmd.none)
+    CheckImage ->
+      ( model, checkImageData "starry-night.jpg" )
+    GetImage x ->
+      ( {model | img = x  }, Cmd.none )
 
 port checkImageData : String -> Cmd msg
 
