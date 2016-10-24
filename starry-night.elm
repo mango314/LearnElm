e133aa1b9584eca41c58d70fc13ba4896851e898
john mangual <john.mangual@gmail.com>
2016-10-23 22:06:42 -0400

    very crude shifting of mouse position

--- starry-night.elm
+++ starry-night.elm
@@ -7,7 +7,9 @@ import Html.Attributes as Html exposing (style, src, width, height)
 import Mouse exposing (Position)
 
 import Svg exposing (rect, svg)
-import Svg.Attributes as Svg exposing (width, height, viewBox, x, y)
+import Svg.Attributes as Svg exposing (width, height, viewBox, x, y, fill)
+
+import Array exposing (..)
 
 
 main =
@@ -20,10 +22,12 @@ main =
 subscriptions: Model -> Sub Msg
 subscriptions model = Sub.batch [ Mouse.moves MouseLocation, getImageData GetImage  ]
 
-type alias Model = { n: Int, x: Int, y: Int, p: Bool, img: List Int }
+-- http://package.elm-lang.org/packages/elm-lang/core/4.0.5/Array
+-- http://package.elm-lang.org/packages/elm-lang/core/4.0.5/List
+type alias Model = { n: Int, x: Int, y: Int, p: Bool, img: Array Int }
 
 init : ( Model, Cmd Msg )
-init = ( { n = 0, x = 0, y = 0, p= False , img = []} , Cmd.none  )
+init = ( { n = 0, x = 0, y = 0, p= False , img = Array.empty} , Cmd.none  )
 
 buttonStyle : Attribute msg
 buttonStyle = style [ ("width", "25px"), ("display", "inline-block") ]
@@ -42,9 +46,28 @@ display model =
 -- Svg.width  : String -> Attribute Msg
 -- Html.width : Int    -> Attribute Msg
 
+toHex: Maybe Int -> String
+toHex x = case x of
+  Just a -> toString a
+  Nothing -> "*"
+
+
+
+getR: Model -> String
+getR model = toHex  <| get ( model.x*4 + 500*4*model.y ) model.img
+
+getG: Model -> String
+getG model = toHex  <| get ( model.x*4 + 500*4*model.y + 1 ) model.img
+
+getB: Model -> String
+getB model = toHex  <| get ( model.x*4 + 500*4*model.y + 2 ) model.img
+
+getColor: Model -> String
+getColor model = "rgb(" ++ (getR model) ++ "," ++ (getG model) ++ "," ++ (getB model) ++ ")"
+
 square: Model -> Html Msg
 square model = svg [ Svg.width "20", Svg.height "20", Svg.viewBox "0 0 20 20"] [
-  rect [ Svg.x "0", Svg.y "0", Svg.width "20", Svg.height "20" ] [] ]
+  rect [ Svg.x "0", Svg.y "0", Svg.width "20", Svg.height "20", Svg.fill <| getColor model] [] ]
 
 view: Model -> Html Msg
 view model =
@@ -55,15 +78,17 @@ view model =
       , span [ numStyle ] [ text (toString model.n) ]
       , button [ onClick Increment, buttonStyle ] [ text "+" ]
       , div [ style [ ("width", "200px"), ("text-align", "center"), ("display", "inline-block")] ] [ display model ]
-      , div [ style [ ("width", "125px"), ("text-align", "center"), ("display", "inline-block")] ] [text <| "Mouse Over: " ++ (toString model.p) ]
+--      , div [ style [ ("width", "100px"), ("text-align", "center"), ("display", "inline-block")] ] [text <| "Mouse Over: " ++ (toString model.p) ]
 --      , div [ style [ ("width", "125px"), ("text-align", "center"), ("display", "inline-block")] ] [text <| "Picture: " ++ (toString model.img) ]
-      , div [ style [ ("width", "125px"), ("text-align", "center"), ("display", "inline-block")] ] [ square model ]
+        , div [ style [ ("width", "100px"), ("text-align", "center"), ("display", "inline-block")] ]
+        [text <| toHex  <| get ( model.x*4 + 500*4*model.y ) model.img  ]
+      , div [ style [ ("width", "75px"), ("text-align", "center"), ("display", "inline-block")] ] [ square model ]
       , button [onClick CheckImage ] [ text "pixelate"]
       ] ,
     div [ Html.width 500, style [ ("float", "left")]  ] [img [ src "starry-night.jpg", Html.width 500, Html.height 300,  onMouseOver ShowLocation, onMouseOut HideLocation] [] ]
     ]
 
-type Msg = Increment | Decrement | MouseLocation Position | ShowLocation | HideLocation | CheckImage | GetImage ( List Int )
+type Msg = Increment | Decrement | MouseLocation Position | ShowLocation | HideLocation | CheckImage | GetImage ( Array Int )
 
 --showLocation =
 --  case msg of
@@ -79,7 +104,7 @@ update msg model =
     Decrement ->
       ( {model | n = model.n - 1 }, Cmd.none)
     MouseLocation pt->
-      ( {model| x = pt.x, y = pt.y} , Cmd.none )
+      ( {model| x = pt.x - 10, y = pt.y - 35} , Cmd.none )
     ShowLocation ->
       ( {model | p  = True } , Cmd.none)
     HideLocation ->
@@ -91,4 +116,4 @@ update msg model =
 
 port checkImageData : String -> Cmd msg
 
-port getImageData: (List Int -> msg ) -> Sub msg
+port getImageData: (Array Int -> msg ) -> Sub msg
