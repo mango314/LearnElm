7151e8dac6b0c260ab959d87d9c0692c3e2d338b
john mangual <john.mangual@gmail.com>
2016-11-18 14:20:19 -0500

    not functional

--- jeudetaquin.elm
+++ jeudetaquin.elm
@@ -3,29 +3,32 @@
 -- a randomly thrown ball to fall inside a circle
 import Html exposing (Html, button, div, text, beginnerProgram)
 import Html.Events exposing (onClick)
+import Html.Attributes exposing (style)
 
 import Svg exposing (svg, rect, line, circle, Svg)
 import Svg.Attributes exposing ( width, height, fill, stroke, x1, x2, y1, y2, strokeWidth, cx, cy, r)
 
-import Random
+import Random exposing (list, pair, float)
 
 main =
   Html.program { init = init  , view = view, update = update , subscriptions = subscriptions}
 
 
-type alias Model = { pts: List Int }
+type alias Model = { pts: List Point }
 
-type Msg = Generate | NewPoints ( List Int )
+type Point = (Float Float)
+
+type Msg = Generate | NewPoints ( List Point )
 
 init : (Model, Cmd Msg)
 init = ( Model [], Cmd.none)
 
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
   case msg of
     Generate ->
-      (model, Random.generate NewPoints ( Random.list ( Random.int 1 19 ) ) )
+      (model, Random.generate NewPoints ( list 19 <| pair (float 0 1 ) (float 0 1 ) ) )
     NewPoints x ->
       ( Model x, Cmd.none )
 
@@ -41,16 +44,17 @@ h (a, b) =  circle [ cx ( toString <| (a+1)*25 ) , cy (toString <| (b+1)*25) , r
 view: Model -> Html Msg
 view model =
   div []
-    [ div []
+    [ div [style [ ("width", "500") ] ]
       [ text "Go Board",
-        button [onClick Generate] [ text "new pts" ]
+        button [onClick Generate, style [ ("width", "75px") ] ] [ text "new pts" ]
       ]
     , div [] [ svg [ width "500", height "500" ] <|
         [ rect [ width "500", height "500", fill "#DCFFB4"] []
         ] ++
         ( List.map (\c -> f 0 c) <| List.range 0 18 ) ++
         ( List.map (\c -> g c 0) <| List.range 0 18 ) ++
-        ( List.map h [ (3,3), (3,9), (3,15), (9,3), (9,9), (9,15), (15,3), (15,9), (15,15) ] )
+        ( List.map h [ (3,3), (3,9), (3,15), (9,3), (9,9), (9,15), (15,3), (15,9), (15,15) ] ) ++
+        ( List.map h model.pts )
       ]
     ]
 
