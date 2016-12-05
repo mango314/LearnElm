a58d964779abf9227a366f8f2746e54d44b8b581
john mangual <john.mangual@gmail.com>
2016-12-05 10:27:22 -0900

    new function f for brevity and structure

--- subway.elm
+++ subway.elm
@@ -1,7 +1,7 @@
-import Html exposing ( Html, button, div, text, h3, span)
+import Html exposing ( Html, button, div, text, h2, span)
 import Html.Events exposing (onClick)
 import Html.Attributes exposing (style)
-import Svg exposing (svg, rect, circle, text_)
+import Svg exposing (Svg, svg, rect, circle, text_)
 import Svg.Attributes exposing (height, width, viewBox,x,y, width, height, fill, cx, cy, r, fontFamily)
 
 main = Html.beginnerProgram { model = 0, view = view, update = update }
@@ -17,56 +17,61 @@ update msg model =
 dTrainColor : String
 dTrainColor = "#FF6319"
 
+view : Int -> Html Msg
 view model =
   div []
     [ div [ style [("backgroundColor", "#EEEEEE" ), ("width", "500px"), ("height", "36pt"), ("text-align", "center") ]]
-      [ h3 [ style [("font-family", "Helvetica")]] [ text "Subway Stations of New York ", span [ style [("color", dTrainColor )]] [ text "D"], text " Line"]
+      [ h2 [ style [("font-family", "Helvetica"), ("line-height", "36pt")]] [ text "Subway Stations of New York ", span [ style [("color", dTrainColor )]] [ text "D"], text " Line"]
       ]
     , svg [ width "500", height "1200", viewBox "0 0 500 1200"] <|
       [ rect [ x "10", y "10", width "10", height "1200", fill "#FF6319" ] []
       ] ++ trainStations
     ]
 
+f : Int -> String -> List ( Svg Msg )
+f yt station = [ circle [ cx "15", cy (toString   yt ), r "4", fill "#000000"] [] , text_ [ fontFamily "Helvetica", x "45", y (toString <|   yt + 5)] [ text station ] ]
+
+
 trainStations : List ( Html Msg )
 trainStations =
-  [ circle [ cx "15", cy (toString   20), r "4", fill "#000000"] [] , text_ [ fontFamily "Helvetica", x "45", y (toString <|   20 + 5)] [ text "205 St" ]
-  , circle [ cx "15", cy (toString   50), r "4", fill "#000000"] [] , text_ [ fontFamily "Helvetica", x "45", y (toString <|   50 + 5)] [ text "Bedford Park Blvd" ]
-  , circle [ cx "15", cy (toString   80), r "4", fill "#000000"] [] , text_ [ fontFamily "Helvetica", x "45", y (toString <|   80 + 5)] [ text "Kingsbridge Rd" ]
-  , circle [ cx "15", cy (toString  110), r "4", fill "#000000"] [] , text_ [ fontFamily "Helvetica", x "45", y (toString <|  110 + 5)] [ text "Fordham Rd" ]
-  , circle [ cx "15", cy (toString  140), r "4", fill "#000000"] [] , text_ [ fontFamily "Helvetica", x "45", y (toString <|  140 + 5)] [ text "182-183 Sts" ]
-  , circle [ cx "15", cy (toString  170), r "4", fill "#000000"] [] , text_ [ fontFamily "Helvetica", x "45", y (toString <|  170 + 5)] [ text "Tremont Ave" ]
-  , circle [ cx "15", cy (toString  200), r "4", fill "#000000"] [] , text_ [ fontFamily "Helvetica", x "45", y (toString <|  200 + 5)] [ text "174-175 St" ]
-  , circle [ cx "15", cy (toString  230), r "4", fill "#000000"] [] , text_ [ fontFamily "Helvetica", x "45", y (toString <|  230 + 5)] [ text "170 St" ]
-  , circle [ cx "15", cy (toString  260), r "4", fill "#000000"] [] , text_ [ fontFamily "Helvetica", x "45", y (toString <|  260 + 5)] [ text "167 St" ]
-  , circle [ cx "15", cy (toString  290), r "4", fill "#000000"] [] , text_ [ fontFamily "Helvetica", x "45", y (toString <|  290 + 5)] [ text "161 St - Yankee Stadium" ]
-  , circle [ cx "15", cy (toString  320), r "4", fill "#000000"] [] , text_ [ fontFamily "Helvetica", x "45", y (toString <|  320 + 5)] [ text "155 St" ]
-  , circle [ cx "15", cy (toString  350), r "4", fill "#000000"] [] , text_ [ fontFamily "Helvetica", x "45", y (toString <|  350 + 5)] [ text "145 St" ]
-  , circle [ cx "15", cy (toString  380), r "4", fill "#000000"] [] , text_ [ fontFamily "Helvetica", x "45", y (toString <|  380 + 5)] [ text "125 St" ]
-  , circle [ cx "15", cy (toString  410), r "4", fill "#000000"] [] , text_ [ fontFamily "Helvetica", x "45", y (toString <|  410 + 5)] [ text "59 St - Columbus Circle" ]
-  , circle [ cx "15", cy (toString  440), r "4", fill "#000000"] [] , text_ [ fontFamily "Helvetica", x "45", y (toString <|  440 + 5)] [ text "7 Avenue" ]
-  , circle [ cx "15", cy (toString  470), r "4", fill "#000000"] [] , text_ [ fontFamily "Helvetica", x "45", y (toString <|  470 + 5)] [ text "47-50 Sts - Rockefeller Center" ]
-  , circle [ cx "15", cy (toString  500), r "4", fill "#000000"] [] , text_ [ fontFamily "Helvetica", x "45", y (toString <|  500 + 5)] [ text "42 St - Bryant Park" ]
-  , circle [ cx "15", cy (toString  530), r "4", fill "#000000"] [] , text_ [ fontFamily "Helvetica", x "45", y (toString <|  530 + 5)] [ text "34 St - Herald Sq" ]
-  , circle [ cx "15", cy (toString  560), r "4", fill "#000000"] [] , text_ [ fontFamily "Helvetica", x "45", y (toString <|  560 + 5)] [ text "W 4 St - Washington Square" ]
-  , circle [ cx "15", cy (toString  590), r "4", fill "#000000"] [] , text_ [ fontFamily "Helvetica", x "45", y (toString <|  590 + 5)] [ text "Broadway-Lafayette St / Bleeker St" ]
-  , circle [ cx "15", cy (toString  620), r "4", fill "#000000"] [] , text_ [ fontFamily "Helvetica", x "45", y (toString <|  620 + 5)] [ text "Grand St" ]
-  , circle [ cx "15", cy (toString  650), r "4", fill "#000000"] [] , text_ [ fontFamily "Helvetica", x "45", y (toString <|  650 + 5)] [ text "Dekalb Ave" ]
-  , circle [ cx "15", cy (toString  680), r "4", fill "#000000"] [] , text_ [ fontFamily "Helvetica", x "45", y (toString <|  680 + 5)] [ text "Atlantic Ave / Barclays Center" ]
-  , circle [ cx "15", cy (toString  710), r "4", fill "#000000"] [] , text_ [ fontFamily "Helvetica", x "45", y (toString <|  710 + 5)] [ text "Union St" ]
-  , circle [ cx "15", cy (toString  740), r "4", fill "#000000"] [] , text_ [ fontFamily "Helvetica", x "45", y (toString <|  740 + 5)] [ text "9 St" ]
-  , circle [ cx "15", cy (toString  770), r "4", fill "#000000"] [] , text_ [ fontFamily "Helvetica", x "45", y (toString <|  770 + 5)] [ text "Prospect Ave" ]
-  , circle [ cx "15", cy (toString  800), r "4", fill "#000000"] [] , text_ [ fontFamily "Helvetica", x "45", y (toString <|  800 + 5)] [ text "25 St" ]
-  , circle [ cx "15", cy (toString  830), r "4", fill "#000000"] [] , text_ [ fontFamily "Helvetica", x "45", y (toString <|  830 + 5)] [ text "36 St" ]
-  , circle [ cx "15", cy (toString  860), r "4", fill "#000000"] [] , text_ [ fontFamily "Helvetica", x "45", y (toString <|  860 + 5)] [ text "9 Ave" ]
-  , circle [ cx "15", cy (toString  890), r "4", fill "#000000"] [] , text_ [ fontFamily "Helvetica", x "45", y (toString <|  890 + 5)] [ text "Fort Hamilton Parkway" ]
-  , circle [ cx "15", cy (toString  920), r "4", fill "#000000"] [] , text_ [ fontFamily "Helvetica", x "45", y (toString <|  920 + 5)] [ text "50 St" ]
-  , circle [ cx "15", cy (toString  950), r "4", fill "#000000"] [] , text_ [ fontFamily "Helvetica", x "45", y (toString <|  950 + 5)] [ text "55 St" ]
-  , circle [ cx "15", cy (toString  980), r "4", fill "#000000"] [] , text_ [ fontFamily "Helvetica", x "45", y (toString <|  980 + 5)] [ text "62 St" ]
-  , circle [ cx "15", cy (toString 1010), r "4", fill "#000000"] [] , text_ [ fontFamily "Helvetica", x "45", y (toString <| 1010 + 5)] [ text "71 St" ]
-  , circle [ cx "15", cy (toString 1040), r "4", fill "#000000"] [] , text_ [ fontFamily "Helvetica", x "45", y (toString <| 1040 + 5)] [ text "18 Ave" ]
-  , circle [ cx "15", cy (toString 1070), r "4", fill "#000000"] [] , text_ [ fontFamily "Helvetica", x "45", y (toString <| 1070 + 5)] [ text "20 Ave" ]
-  , circle [ cx "15", cy (toString 1100), r "4", fill "#000000"] [] , text_ [ fontFamily "Helvetica", x "45", y (toString <| 1100 + 5)] [ text "Bay Parkway" ]
-  , circle [ cx "15", cy (toString 1130), r "4", fill "#000000"] [] , text_ [ fontFamily "Helvetica", x "45", y (toString <| 1130 + 5)] [ text "25 Ave" ]
-  , circle [ cx "15", cy (toString 1160), r "4", fill "#000000"] [] , text_ [ fontFamily "Helvetica", x "45", y (toString <| 1160 + 5)] [ text "Bay 50 St" ]
-  , circle [ cx "15", cy (toString 1190), r "4", fill "#000000"] [] , text_ [ fontFamily "Helvetica", x "45", y (toString <| 1190 + 5)] [ text "Coney Island - Stillwell Ave" ]
+  (f   20 "205 St" ) ++
+  (f   50 "Bedford Park Blvd" ) ++
+  (f   80 "Kingsbridge Rd" ) ++
+  (f 110  "Fordham Rd" ) ++
+  (f 140 "182-183 Sts" ) ++
+  (f 170 "Tremont Ave" ) ++
+  (f 200  "174-175 St" ) ++
+  (f 230 "170 St" ) ++
+  (f 260  "167 St" )
+  (f  290  "161 St - Yankee Stadium" ) ++
+  (f  320    "155 St" ) ++
+  (f  350     "145 St" ) ++
+  (f  380     "125 St" ) ++
+  (f  410     "59 St - Columbus Circle" ) ++
+  (f  440     "7 Avenue" ) ++
+  (f  470     "47-50 Sts - Rockefeller Center" ) ++
+  (f  500     "42 St - Bryant Park" ) ++
+  (f  530     "34 St - Herald Sq" ) ++
+  (f  560     "W 4 St - Washington Square" ) ++
+  (f  590     "Broadway-Lafayette St / Bleeker St" ) ++
+  (f  620     "Grand St" ) ++
+  (f  650     "Dekalb Ave" ) ++
+  (f  680     "Atlantic Ave / Barclays Center" ) ++
+  (f  710     "Union St" ) ++
+  (f  740     "9 St" ) ++
+  (f  770     "Prospect Ave" ) ++
+  (f  800     "25 St" ) ++
+  (f  830     "36 St" ) ++
+  (f  860     "9 Ave" ) ++
+  (f  890     "Fort Hamilton Parkway" ) ++
+  (f  920     "50 St" ) ++
+  (f  950     "55 St" ) ++
+  (f  980     "62 St" ) ++
+  (f 1010    "71 St" ) ++
+  (f 1040    "18 Ave" ) ++
+  (f 1070    "20 Ave" ) ++
+  (f 1100    "Bay Parkway" ) ++
+  (f 1130   "25 Ave" ) ++
+  (f 1160   "Bay 50 St" ) ++
+  (f 1190    "Coney Island - Stillwell Ave" )
   ]
