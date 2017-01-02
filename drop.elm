6a9e2e2d19acc9f95c1238a2b4bd3b15b0d4bd5a
john mangual <john.mangual@gmail.com>
2017-01-02 12:37:38 -0900

    stacking game!

--- drop.elm
+++ drop.elm
@@ -31,18 +31,19 @@ update msg model  =
           Nothing -> ( Model <| (List.take x model.stack ) ++ [0]    ++ (List.drop (x+1) model.stack ) , Cmd.none)
 
 f : Int -> Html Msg
-f x = div [ style [("width", "25px"),("display", "inline-block")] ] [ button [ onClick <| Drop (x-1)  ] [ text (toString x) ] ]
-
+--f x = div [ style [("width", "25px"),("display", "inline-block")] ] [ button [ onClick <| Drop (x-1)  ] [ text (toString x) ] ]
+f x = div [ style [("width", "25px"),("display", "inline-block")] ] [ button [ onClick <| Drop (x-1)  ] [ text "■" ] ]
 g : Int -> Html Msg
 g x = div [ style [("width", "25px"), ("display", "inline-block"), ("text-align", "center")] ] [ text ( toString x )]
 
 h : (Int, Int) -> Svg Msg
-h (a,b) = rect [ width "20", height "20", x <| toString (25*b) , y <| toString (25*a) , fill "red" ] []
+h (a,b) = rect [ width "20", height "20", x <| toString (25*b) , y <| toString (125 - (25*a)) , fill "#EB895B" ] []
 
+-- some style problems.  why not just have a fixed set of pixels (e.g. 8 by 10) and change the colors?
 view : Model -> Html Msg
 view model =
-  div []
-  [ svg [] <| List.map h <| List.concat <| List.map (\b -> List.map (\a -> (a,b)) <| List.range 0 (b-1) ) <| model.stack
+  div [ style [ ("margin", "5px") ] ]
+  [ svg [ height "150"] <| List.map h <| List.concat <| List.map (\(a,b) -> List.map (\c -> (c,a) )  (List.range 0 (b-1)  ) ) <| List.map2 (,) (List.range 0 7) model.stack
   , div [] <| List.map g model.stack
   , div [] <| List.map f <| List.range 1 8
   ]
