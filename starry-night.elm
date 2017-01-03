be0755ff7fc73cb9605c572b39a90f05dfc7195c
john mangual <john.mangual@gmail.com>
2017-01-03 14:22:00 -0900

    v0.18 compliance + other issues ( boring )

--- starry-night.elm
+++ starry-night.elm
@@ -1,15 +1,16 @@
 port module VanGogh exposing (..)
 
-import Html exposing (div, span, button, text, Attribute, img, Html)
-import Html.App exposing (program)
+import Html exposing (div, span, button, text, Attribute, img, Html, program)
 import Html.Events exposing (onClick, onMouseOver, onMouseOut)
 import Html.Attributes as Html exposing (style, src, width, height)
 import Mouse exposing (Position)
 
-import Svg exposing (rect, svg)
+import Svg exposing (rect, svg, Svg)
 import Svg.Attributes as Svg exposing (width, height, viewBox, x, y, fill)
 
 import Array exposing (..)
+
+import Time exposing (Time, second)
 
 
 main =
@@ -25,6 +26,9 @@ subscriptions model = Sub.batch [ Mouse.moves MouseLocation, getImageData GetIma
 -- http://package.elm-lang.org/packages/elm-lang/core/4.0.5/Array
 -- http://package.elm-lang.org/packages/elm-lang/core/4.0.5/List
 type alias Model = { n: Int, x: Int, y: Int, p: Bool, img: Array Int }
+
+type QuadTree a = Empty | Node a (QuadTree a) (QuadTree a) (QuadTree a) (QuadTree a)
+
 
 init : ( Model, Cmd Msg )
 init = ( { n = 0, x = 0, y = 0, p= False , img = Array.empty} , Cmd.none  )
@@ -49,49 +53,53 @@ display model =
 toHex: Maybe Int -> String
 toHex x = case x of
   Just a -> toString a
-  Nothing -> "*"
+  Nothing -> "255"
 
 
+getR: Array Int -> (Int, Int) -> String
+getR image (x,y) = toHex  <| get ( x*4 + 500*4*y     ) image
 
-getR: Model -> String
-getR model = toHex  <| get ( model.x*4 + 500*4*model.y ) model.img
+getG: Array Int -> (Int, Int) -> String
+getG image (x,y) = toHex  <| get ( x*4 + 500*4*y + 1 ) image
 
-getG: Model -> String
-getG model = toHex  <| get ( model.x*4 + 500*4*model.y + 1 ) model.img
+getB: Array Int -> (Int, Int) -> String
+getB image (x,y) = toHex  <| get ( x*4 + 500*4*y + 2 ) image
 
-getB: Model -> String
-getB model = toHex  <| get ( model.x*4 + 500*4*model.y + 2 ) model.img
+getColor: Array Int -> (Int,Int) -> String
+getColor image x = "rgb(" ++ (getR image x) ++ "," ++ (getG image x) ++ "," ++ (getB image x) ++ ")"
 
-getColor: Model -> String
-getColor model = "rgb(" ++ (getR model) ++ "," ++ (getG model) ++ "," ++ (getB model) ++ ")"
 
 square: Model -> Html Msg
-square model = svg [ Svg.width "20", Svg.height "20", Svg.viewBox "0 0 20 20"] [
-  rect [ Svg.x "0", Svg.y "0", Svg.width "20", Svg.height "20", Svg.fill <| getColor model] [] ]
+square model =
+  if model.p then
+    svg [ Svg.width "20", Svg.height "20", Svg.viewBox "0 0 20 20"] [ rect [ Svg.x "0", Svg.y "0", Svg.width "20", Svg.height "20", Svg.fill <| getColor model.img (model.x, model.y)] [] ]
+  else
+    svg [ Svg.width "20", Svg.height "20", Svg.viewBox "0 0 20 20"] [ rect [ Svg.x "0", Svg.y "0", Svg.width "20", Svg.height "20", Svg.fill "rgb(255,255,255)"            ] [] ]
+
+f : Model -> Int -> Svg Msg
+f model t = rect [ Svg.x "0", Svg.y "0", Svg.width "10", Svg.height "10", Svg.fill "#A0A0A0"] []
+
+pixel : Model -> Svg Msg
+pixel model = svg [ Svg.width "500" , Svg.height "500" ]
+  <| Array.toList <| Array.map (\x -> f model x ) <| Array.fromList
+  <| List.map (\x -> 10000*x) <| List.range 0 <| (Array.length model.img ) // 10000
 
 view: Model -> Html Msg
 view model =
 
   div [ Html.width 500 ] [
     div [   ]
-      [ button [ onClick Decrement, buttonStyle ] [ text "-" ]
-      , span [ numStyle ] [ text (toString model.n) ]
-      , button [ onClick Increment, buttonStyle ] [ text "+" ]
-      , div [ style [ ("width", "200px"), ("text-align", "center"), ("display", "inline-block")] ] [ display model ]
---      , div [ style [ ("width", "100px"), ("text-align", "center"), ("display", "inline-block")] ] [text <| "Mouse Over: " ++ (toString model.p) ]
---      , div [ style [ ("width", "125px"), ("text-align", "center"), ("display", "inline-block")] ] [text <| "Picture: " ++ (toString model.img) ]
-        , div [ style [ ("width", "100px"), ("text-align", "center"), ("display", "inline-block")] ]
-        [text <| toHex  <| get ( model.x*4 + 500*4*model.y ) model.img  ]
-      , div [ style [ ("width", "75px"), ("text-align", "center"), ("display", "inline-block")] ] [ square model ]
+      [ div [ style [ ("width", "200px"), ("text-align", "left")  , ("display", "inline-block")] ] [ display model ]
+      , div [ style [ ("width", "100px"), ("text-align", "left")  , ("display", "inline-block")] ] [ text <| getColor model.img (model.x, model.y)  ]
+      , div [ style [ ("width", "75px") , ("text-align", "center"), ("display", "inline-block")] ] [ square model ]
       , button [onClick CheckImage ] [ text "pixelate"]
-      ] ,
-    div [ Html.width 500, style [ ("float", "left")]  ] [img [ src "starry-night.jpg", Html.width 500, Html.height 300,  onMouseOver ShowLocation, onMouseOut HideLocation] [] ]
+      ]
+    , div [ Html.width 500, style [ ("float", "left")]  ] [img [ src "starry-night.jpg", Html.width 500, Html.height 300,  onMouseOver ShowLocation, onMouseOut HideLocation] [] ]
+    , div [ Html.width 500 ] [ pixel model ]
     ]
 
-type Msg = Increment | Decrement | MouseLocation Position | ShowLocation | HideLocation | CheckImage | GetImage ( Array Int )
+type Msg = MouseLocation Position | ShowLocation | HideLocation | CheckImage | GetImage ( Array Int )
 
---showLocation =
---  case msg of
 
 
 -- our example could be read mouse locaation (over image) and return color of pixel at mouse location
@@ -99,10 +107,6 @@ type Msg = Increment | Decrement | MouseLocation Position | ShowLocation | HideL
 update: Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
   case msg of
-    Increment ->
-      ( {model | n = model.n + 1 } , Cmd.none)
-    Decrement ->
-      ( {model | n = model.n - 1 }, Cmd.none)
     MouseLocation pt->
       ( {model| x = pt.x - 10, y = pt.y - 35} , Cmd.none )
     ShowLocation ->
