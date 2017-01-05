98d70123502d640aeeec9ad1c6c8606888b9faca
john mangual <john.mangual@gmail.com>
2017-01-05 09:17:54 -0900

    empty file

---
+++ sticky-squares.elm
@@ -0,0 +1,42 @@
+import Html exposing (Html, div)
+
+-- the goal of stacky squares" is an excuse
+-- to develops arrays from scratch
+-- Elm arrays are known to be "unsafe"
+-- most programmer don't think hard about the array
+-- (or list type) they are using.
+-- in fact we can implement our own array based
+-- on a tree.  Our screen is a 2D array and we need
+-- to be aware of neighbor relationships. And
+-- to move objects from one place to another.
+
+-- this is also to prove we can write Elm Array'
+-- type without every writing native JS code
+
+-- lastly please don't let the term "ballistic deposition"
+-- scare you it is just a bunch of stacky squares
+
+main = Html.program
+  { init = init
+  , view = view
+  , update = update
+  , subscriptions = subscriptions }
+
+
+-- Model
+
+type alias Model = { n : Int }
+
+type Msg = Go
+
+init : ( Model, Cmd Msg )
+init = ( Model 0, Cmd.none )
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model = (model, Cmd.none)
+
+view : Model -> Html Msg
+view model = div [] []
+
+subscriptions : Model -> Sub Msg
+subscriptions model = Sub.none
