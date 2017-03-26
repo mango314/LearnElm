3ea59942142b1107f624bbad4fed78162cf60e5c
john mangual <john.mangual@gmail.com>
2017-03-26 13:24:29 -0400

    starting to look good

--- operators.elm
+++ operators.elm
@@ -18,9 +18,30 @@ update msg model =
 
 style1 = style [ ("font-family" , "Helvetica"), ("width", "750px"), ("margin-left", "1%"), ("text-align", "justify")]
 
-styleop = style [ ("font-size", "50px") ]
+styleop = style [ ("font-size", "50px"), ("font-weight", "100") ]
 
 styleop1 = style [ ("font-size", "40px") ]
+
+h1_ url x = case url of
+  Just url_  -> h1 [ styleop ] [ a [ href url_
+                     , style [( "color", "#30A3E8"), ("text-decoration", "none")]
+                     ] [ code [] [ text x ] ] ]
+  Nothing -> h1 [ styleop ] [ a [ href ""
+                     , style [( "color", "#30A3E8"), ("text-decoration", "none")]
+                     ] [ code [] [ text x ] ] ]
+
+h1_fn  url x = case url of
+  Just url_  -> h1 [ styleop ] [ a [ href url_
+                     , style [( "color", "#6DF22B"), ("text-decoration", "none")]
+                     ] [ code [] [ text x ] ] ]
+  Nothing -> h1 [ styleop ] [ a [ href ""
+                     , style [( "color", "#6DF22B"), ("text-decoration", "none")]
+                     ] [ code [] [ text x ] ] ]
+
+-- https://developer.mozilla.org/en-US/docs/Web/CSS/font-weight
+boldText x = span [ style [("font-weight", "bold")] ] [ text x]
+
+code_ x = code [] [ text x ]
 
 view model =
   div [ style1 ]
@@ -28,13 +49,13 @@ view model =
     , p  [] [ text "Why does HTML have to be the way it is?  Reflex-Dom offers an alternative way to code HTML "
             , text "with additional rigor provided from the Haskell programming language. "
             , text "We could imagine programming websites without ever mentioning the "
-            , code [] [ text "<p>" ]
+            , code_ "<p>"
             , text " and "
-            , code [] [ text "<h1>"]
+            , code_ "<h1>"
             , text " symbols.  We can roll our own."
             ]
     , p [] [ text "Haskell and Reflex-Dom pose a "
-           , span [style [("font-weight", "bold")] ] [ text "storytelling challenge"]
+           , boldText "storytelling challenge"
            , text " as the coder is being asked to put together a peculiar formulation of HTML with exotic types "
            , text "and unusual operators, invoking the full force of monads, applicatives and lenses. "
            , text "A great question to ask is whether all of this is really necessary? "
@@ -50,9 +71,7 @@ svg =  do
            , text " That's too bad.  We are going to take a simple thing and make it uncomprehensible.  When we are done, "
            , text "nobody will understand so much as to tie their own shoes in this language."
            ]
-    , h1 [ styleop ] [ a [ href "https://hackage.haskell.org/package/base-4.9.1.0/docs/Data-Function.html"
-                         , style [( "color", "#30A3E8") ]
-                         ] [ code [] [ text "&" ] ] ]
+    , h1_ ( Just "https://hackage.haskell.org/package/base-4.9.1.0/docs/Data-Function.html" ) "&"
     , p [] [ text "the and operator has type blah blah blah"
            , text "it is very similar to "
            , code [] [ text "$"]
@@ -62,30 +81,35 @@ svg =  do
            , code [] [ pre [] [ text "(&) :: a -> (a -> b) -> b" ] ]
            , text "these have to do with the math notion of associativity and whether we evaluate function on the right or on the left"
            ]
-    , h1 [ styleop ] [ code [] [  text ".~"] ]
+    , h1_ Nothing ".~"
     , p [] [ text "this connective has to do with lenses" ]
-    , h1 [ styleop ] [ code [] [  text "=:"  ] ]
+    , h1_ Nothing "=:"
     , p [] [ text "Not to be confused with ", code [] [text ":="], text " or ", code [] [text "::"], text " this operator is about Haskell Maps"
            , text " ''Map'' is such an ambiguous word anyway."]
-    , h1 [ styleop ] [ code [] [ text "=>" ] ]
+    , h1_ Nothing "=>"
     , p [ ] [ text "This is called a "
-            , span [ style [("font-weight", "bold")] ] [ text "type constraint"]
+            , boldText "type constraint"
             , text " and the type constraint is that "
             , code [] [ text "m"]
             , text " is a "
             , code [] [ text "MonadWidget" ]
             , text "."
             ]
-    , h1 [ styleop ] [ code [] [ text "()" ] ]
+    , h1_ Nothing "()"
     , p [] [ text """I must double check.  This is basically the empty set.  In Haskell we have a symbol for "nothing"."""]
-    , h1 [ styleop1 ] [ code [] [ text "blank"] ]
+    , h1_fn Nothing "blank"
+    , p [] [ text  "There really isn't much to blank.  It is has type "
+           , code_ "blank :: Monad m => m ()"
+           , text  """This is a way of saying "Nothing" in the category of """
+           , boldText "Widgets"
+           , text "."
+           ]
+    , h1_fn Nothing "def"
     , p [] [ text "this connective has to do with lenses" ]
-    , h1 [ styleop1 ] [ code [] [ text "def"] ]
+    , h1_fn Nothing "namespace"
     , p [] [ text "this connective has to do with lenses" ]
-    , h1 [ styleop1 ] [ code [] [ text "namespace"] ]
+    , h1_fn Nothing """namespace .~ Just "xyz" """
     , p [] [ text "this connective has to do with lenses" ]
-    , h1 [ styleop1 ] [ code [] [ text "def & namespace"] ]
-    , p [] [ text "this connective has to do with lenses" ]
-    , h1 [ styleop1 ] [ code [] [ text "element"] ]
+    , h1_fn Nothing "element"
     , p [] [ text "of all of these, Element is the most involved being lower-level abstraction than elAttr, elDynAttr, etc." ]
     ]
