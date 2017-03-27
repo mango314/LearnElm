5a7afb090888a85f7f59ffb7b0aaee6a138eacbe
john mangual <john.mangual@gmail.com>
2017-03-26 23:36:06 -0400

    contact with the outside world in Haskell

--- button.elm
+++ button.elm
@@ -1,55 +1,82 @@
-import Html exposing (div, span, button, text, Attribute, img, Html)
-import Html.App exposing (program)
-import Html.Events exposing (onClick, onMouseOver)
-import Html.Attributes exposing (style, src, width)
-import Mouse exposing (Position)
+module Operators exposing (..)
 
+import Html exposing (Html, Attribute, button, div, text, h1, p, span, pre, code, a)
+import Html.Events exposing (onClick)
+import Html.Attributes exposing (style, href)
 
 main =
-  program { init = init, view = view, update = update, subscriptions = subscriptions}
+  Html.beginnerProgram { model = 0, view = view, update = update }
 
--- Elm does not save us from CSS it only makes it more manageable
--- https://developer.mozilla.org/en-US/docs/Web/CSS/display
--- https://developer.mozilla.org/en-US/docs/Web/CSS/float
+type Msg = Increment
 
-subscriptions: Model -> Sub Msg
-subscriptions model = Mouse.moves MouseLocation
-
-type alias Model = { n: Int, x: Int, y: Int }
-
-init : ( Model, Cmd Msg )
-init = ( { n = 0, x = 0, y = 0 } , Cmd.none  )
-
-buttonStyle : Attribute msg
-buttonStyle = style [ ("width", "25px") ]
-
-numStyle : Attribute msg
-numStyle = style [ ("width", "25px"), ("float", "left"), ("text-align", "center")]
-
-view: Model -> Html Msg
-view model =
-
-  div [ width 500 ] [
-    div [  ]
-      [ button [ onClick Decrement, buttonStyle, style [ ("float", "left") ] ] [ text "-" ]
-      , span [ numStyle ] [ text (toString model.n) ]
-      , button [ onClick Increment, buttonStyle ] [ text "+" ]
-      , text <| "Mouse Location:" ++ " ( " ++ (toString model.x) ++ "," ++ (toString model.y) ++ " )"
-      ] ,
-    div [  ] [img [ src "starry-night.jpg", width 500 ] [] ]
-    ]
-
-type Msg = Increment | Decrement | MouseLocation Position
-
-
--- our example could be read mouse locaation (over image) and return color of pixel at mouse location
-
-update: Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
   case msg of
     Increment ->
-      ( { x = model.x, y = model.y, n = model.n + 1 }, Cmd.none)
-    Decrement ->
-      ( { x = model.x, y = model.y, n = model.n - 1 }, Cmd.none)
-    MouseLocation pt->
-      ( model, Cmd.none )
+      1 - model
+
+
+style1 = style [ ("font-family" , "Helvetica"), ("width", "750px"), ("margin-left", "1%"), ("text-align", "justify")]
+
+styleop = style [ ("font-size", "30px"), ("font-weight", "100") ]
+
+styleop1 = style [ ("font-size", "20px") ]
+
+h1_ url x = case url of
+  Just url_  -> h1 [ styleop ] [ a [ href url_
+                     , style [( "color", "#30A3E8"), ("text-decoration", "none")]
+                     ] [ code [] [ text x ] ] ]
+  Nothing -> h1 [ styleop ] [ a [ href ""
+                     , style [( "color", "#30A3E8"), ("text-decoration", "none")]
+                     ] [ code [] [ text x ] ] ]
+
+h1_fn  url x = case url of
+  Just url_  -> h1 [ styleop ] [ a [ href url_
+                     , style [( "color", "#6DF22B"), ("text-decoration", "none")]
+                     ] [ code [] [ text x ] ] ]
+  Nothing -> h1 [ styleop ] [ a [ href ""
+                     , style [( "color", "#6DF22B"), ("text-decoration", "none")]
+                     ] [ code [] [ text x ] ] ]
+
+-- https://developer.mozilla.org/en-US/docs/Web/CSS/font-weight
+boldText x = span [ style [("font-weight", "bold")] ] [ text x]
+
+code_ x = code [] [ text x ]
+
+view model =
+  div [ style1 ]
+    [ h1 [] [ text "Button" ]
+    , p  [] [ text "Static webpages are nice, Reflex is a type of FRP (Functional Reactive Programming) "
+            , text "I have no idea how mouse events work in this language: what happens when you click a button? Or a dropdown menu?"
+            ]
+    , p  [] [ text "The code for dropDown.  It is pretty overwhelming.  Here is the code for "
+            , code_ "button"
+            , text "."]
+    , pre [] [ text """button :: DomBuilder t m => Text -> m (Event t ())
+button t = do
+  (e, _) <- element "button" def $ text t
+  return $ domEvent Click e""" ]
+    , p [] [ text "That was easy.  What happens after you click the button? At this point we seem to leave Reflex-Dom"]
+    , pre [] [ text """count :: (Reflex t, MonadHold t m, MonadFix m, Num b) => Event t a -> m (Dynamic t b)
+count e = holdDyn 0 =<< zipListWithEvent const (iterate (+1) 1) e"""]
+    , p []  [ text "and off we go... The exercise could be to combine "
+            , code_ "button"
+            , text " and "
+            , code_ "count"
+            , text " and write to some kind of "
+            , code_ "text"
+            , text " element."
+            ]
+    , p []  [ text "This is our first instance of monads. There are at least two: "
+            , code_ "monadHold"
+            , text " and "
+            , code_ "monadWidget"
+            , text ".  My first impression is that "
+            , code_ "count"
+            , text " has the wrong type.  It should be"
+            , pre [] [ text "m ( Event t a )" ]
+            , text "but we only have "
+            , code_ "Event t ()"
+            , text " maybe we need to use the "
+            , code_ ">>>"
+            , text " symbol."]
+    ]
