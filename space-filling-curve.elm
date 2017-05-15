7606e46eb7a1addb2427c245626785097b92c003
john mangual <john.mangual@gmail.com>
2017-05-15 15:28:23 -0400

    still missing the mark

--- space-filling-curve.elm
+++ space-filling-curve.elm
@@ -4,21 +4,34 @@ import Html.Attributes exposing (style)
 import Svg exposing (Svg, svg, polygon, node, line, g, circle, polyline )
 import Svg.Attributes exposing (points, fill, stroke, strokeWidth, cx, x1, x2, cy, y1, y2, r)
 
+-- TWO STEPS:
+--
+-- design user interface
+-- solve geometry problem
+
 main =
-  Html.beginnerProgram { model = "", view = view, update = update }
+  Html.beginnerProgram { model = Model [(0,0)] (0,0) (0,0) "" , view = view, update = update }
+
+type alias Model = { pathNum: List (Int, Int), start: (Int, Int), end: (Int, Int), pathString : String }
 
 type Msg = Type String
 
+update : Msg -> Model -> Model
 update msg model =
   case msg of
-    Type newPath -> newPath
+    Type x -> { model |  pathString = x }
 
--- integrate the curvatuve of the path
-f : List String -> List (Int, Int)
-f x = [(0,0)]
+next : (Int, Int) -> List String -> (Int, Int)
+next pt seq = pt
 
-calculatePath : String -> List (Int, Int)
-calculatePath str = f <| String.split "" str
+-- integrate the line segments
+f : (Int, Int) -> List String -> List (Int, Int)
+f pt seq = case List.tail seq of
+  Just seq1  -> [ pt ] ++  ( f (next pt seq) seq1 )
+  Nothing -> []
+
+calculatePath : (Int, Int) -> String -> List (Int, Int)
+calculatePath x str = f x <| String.split "" str
 
 writePath : List (Int, Int) -> String
 writePath x = String.join " " <| List.map (\(a,b) -> (toString a) ++ "," ++ (toString b)) x
@@ -27,40 +40,19 @@ dx : Int -> Int
 dx x = 20 + 46 + 92*x
 
 dy : Int -> Int
-dy y = dx y
+dy y = 20 + 46 + 92*y
 
-start : List (Int, Int) -> Svg msg
-start x = case List.head x of
-    Just pt -> circle
-      [ cx <| toString <| dx <| Tuple.first pt
-      , cy <| toString <| dy <| Tuple.second pt
+toCircle : (Int, Int) -> Svg Msg
+toCircle (a,b) = circle
+      [ cx <| toString <| dx <| a
+      , cy <| toString <| dy <| b
       , r <| toString 10, stroke "none"
       , fill "#E8D62A" ] []
-    Nothing -> circle
-      [ cx <| toString <| dx 0
-      , cy <| toString <| dy 0
-      , r <| toString 10
-      , stroke "none"
-      , fill "#E8D62A" ] []
 
--- retrive correct value after iterating over the list of line segments.  return a tuple
-finish : List (Int, Int) -> Svg msg
-finish x = case List.head x of
-    Just pt -> circle
-      [ cx <| toString <| dx <| Tuple.first pt
-      , cy <| toString <| dy <| Tuple.second pt
-      , r <| toString 10, stroke "none"
-      , fill "#E8D62A" ] []
-    Nothing -> circle
-      [ cx <| toString <| dx 0
-      , cy <| toString <| dy 0
-      , r <| toString 10
-      , stroke "none"
-      , fill "#E8D62A" ] []
+toPath : (Int, Int) -> String -> Svg Msg
+toPath x str = g [] [ polyline [ strokeWidth "1", stroke "#000000", points <| writePath <| calculatePath x str ] [] ]
 
-toPath str = g [] [ start <| calculatePath str, polyline [ strokeWidth "1", stroke "#000000", points <| writePath <| calculatePath str ] [], finish <| calculatePath str]
-
-
+view: Model -> Html Msg
 view model =
   div [style [("padding-left", "5px")]]
     [ h1 [ style [("font-family", "Helvetica")] ]
@@ -78,13 +70,27 @@ view model =
           <| List.map (\(a,b) -> (20 + 115*a, 20 + 115*b)) [(0,0),(4,0), (4,4), (0, 4), (0,0)]
         ] []
       , g []
-          <| List.map (\a -> line [ x1 (toString a), x2 (toString a), y1 (toString 20), y2 (toString 480), strokeWidth "1", stroke "#3CE875"  ] [] )
+          <| List.map (\a -> line
+            [ x1 (toString a)
+            , x2 (toString a)
+            , y1 (toString 20)
+            , y2 (toString 480)
+            , strokeWidth "1"
+            , stroke "#3CE875"  ] [] )
           <| List.map (\x -> x)
           <| List.map (\x -> 20 + 92*x ) [1,2,3,4]
       , g []
-          <| List.map (\a -> line [ x1 (toString 20), x2 (toString 480), y1 (toString a), y2 (toString a), strokeWidth "1", stroke "#3CE875"  ] [] )
+          <| List.map (\a -> line
+            [ x1 (toString 20)
+            , x2 (toString 480)
+            , y1 (toString a)
+            , y2 (toString a)
+            , strokeWidth "1"
+            , stroke "#3CE875"  ] [] )
           <| List.map (\x -> x)
           <| List.map (\x -> 20 + 92*x  ) [1,2,3,4]
-      , toPath model
+      , toCircle  model.start
+      , toCircle model.end
+      , toPath model.start model.pathString
       ]
     ]
