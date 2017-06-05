cd304b3e64bc0974cae29d14d1c4f19cf26406e3
john mangual <john.mangual@gmail.com>
2017-06-04 21:17:32 -0400

    some insight about typing out the curve

--- space-filling-curve.elm
+++ space-filling-curve.elm
@@ -2,7 +2,7 @@ import Html exposing (Html, button, div, text, h1, textarea)
 import Html.Events exposing (onClick, onInput)
 import Html.Attributes exposing (style)
 import Svg exposing (Svg, svg, polygon, node, line, g, circle, polyline )
-import Svg.Attributes exposing (points, fill, stroke, strokeWidth, cx, x1, x2, cy, y1, y2, r)
+import Svg.Attributes exposing (points, fill, stroke, strokeWidth, cx, x1, x2, cy, y1, y2, r, fill)
 
 -- TWO STEPS:
 --
@@ -14,21 +14,43 @@ main =
 
 type alias Model = { pathNum: List (Int, Int), start: (Int, Int), end: (Int, Int), pathString : String }
 
-type Msg = Type String
+type Msg = Type String | Left | Right | Up | Down | Restart
+
+alphabet : List String
+alphabet = [ "l", "L", "r", "R", "u", "U", "d", "D" ]
+
+clean : String -> String
+clean x =   ( String.toUpper << String.concat << (\data -> List.filter (\x -> List.member x alphabet ) data ) << (\t -> String.split "" t) ) x
 
 update : Msg -> Model -> Model
 update msg model =
   case msg of
-    Type x -> { model |  pathString = x }
+    Type x  -> { model |  pathString = clean x }
+    Left    -> model
+    Right   -> model
+    Up      -> model
+    Down    -> model
+    Restart -> model
 
+-- do not explicitly say 92
 next : (Int, Int) -> List String -> (Int, Int)
-next pt seq = pt
+next (a,b) seq = case List.head seq of
+  Just "L" -> (a-92,b)
+  Just "R" -> (a+92,b)
+  Just "U" -> (a,b-92)
+  Just "D" -> (a,b+92)
+  Just _   -> (a,b)
+  Nothing  -> (a,b)
 
 -- integrate the line segments
+-- needs significant clean-up
+-- needs to check for self-intersection
+-- check for outside boundary
+-- somewhat opinionated guide you into safe spaces
 f : (Int, Int) -> List String -> List (Int, Int)
 f pt seq = case List.tail seq of
   Just seq1  -> [ pt ] ++  ( f (next pt seq) seq1 )
-  Nothing -> []
+  Nothing -> [ pt ]
 
 calculatePath : (Int, Int) -> String -> List (Int, Int)
 calculatePath x str = f x <| String.split "" str
@@ -50,15 +72,16 @@ toCircle (a,b) = circle
       , fill "#E8D62A" ] []
 
 toPath : (Int, Int) -> String -> Svg Msg
-toPath x str = g [] [ polyline [ strokeWidth "1", stroke "#000000", points <| writePath <| calculatePath x str ] [] ]
+toPath x str = g [] [ polyline [ strokeWidth "3", stroke "#E8D62A", points <| writePath <| calculatePath x str, fill "none"] [] ]
 
 view: Model -> Html Msg
 view model =
   div [style [("padding-left", "5px")]]
     [ h1 [ style [("font-family", "Helvetica")] ]
       [ div [ style [("width", "300"), ("display", "inline-block")] ] [ text "space-filling curves" ]
-      , div [ style [("width", "200"), ("display", "inline-block")] ] [ textarea [ style [("width", "200"), ("float", "right")], onInput Type ] [] ]
+      , div [ style [("width", "200"), ("display", "inline-block")] ] [ textarea [ style [("width", "200"), ("float", "right")], onInput Type ] [ ] ]
       ]
+    , div [  ] [ text  model.pathString ]
     , svg [ style [ ( "width", "500"), ("height", "500"), ("background-color", "#F0F0F0")] ]
       [ polygon
         [ stroke "#3CE875"
@@ -91,6 +114,6 @@ view model =
           <| List.map (\x -> 20 + 92*x  ) [1,2,3,4]
       , toCircle  model.start
       , toCircle model.end
-      , toPath model.start model.pathString
+      , toPath ( (\(a,b) -> (dx a, dy b)) model.start ) model.pathString
       ]
     ]
