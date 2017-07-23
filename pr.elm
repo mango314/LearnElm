91360fc3c368f873cbd25d09c6aef7cdc102735a
JDM <john.mangual@gmail.com>
2017-07-23 16:52:36 -0400

    gymnastics

--- pr.elm
+++ pr.elm
@@ -1,21 +1,34 @@
 import Html exposing (Html, Attribute, button, div, text)
 import Html.Events exposing (onClick)
-import Svg exposing (Svg, svg)
+import Svg exposing (Svg, svg, g, polygon)
+import Svg.Attributes exposing (points)
 
 import Http
-import Json.Decode as Decode exposing (Decoder, at, list, maybe, map, field, float)
+import Json.Decode as Decode exposing (Decoder, at, list, maybe, map, map2, field, float, oneOf, string)
 import Html.Attributes exposing (style)
 
 main =
   Html.program { init = init, view = view, update = update , subscriptions = subscriptions }
 
-type Msg = GetMap ( Result Http.Error   ( List ( Maybe Polygon ) ) ) | AskMap
+type Msg = GetMap ( Result Http.Error ( List ( Maybe Polygon ) ) ) | AskMap
 
 -- INIT
 
 type alias Model   =   { note:  String, polygons: List ( Maybe Polygon ) }
 
-type alias Polygon = { coordinates: List ( List ( List Float )) }
+type alias Polygon =   { name: String, coordinates: List ( List ( List Float )) }
+
+type alias MultiPolygon = { name: String , coordinates: List ( List ( List ( List Float ))) }
+
+-- > type Qqq = Abc String | Xyz Int
+-- > Abc
+-- <function> : String -> Repl.Qqq
+
+
+type Town =  Foo MultiPolygon | Bar Polygon
+
+
+-- type Poly = Muni Polygon | MuniPoly MultiPolygon
 
 init : ( Model, Cmd Msg )
 init = ( Model "PR info" ( [] ) , Cmd.none )
@@ -39,28 +52,41 @@ subscriptions model = Sub.none
 -- VIEW
 
 divStyle : Html.Attribute Msg
-divStyle = style [("width", "250px"), ("display", "inline-block")]
+divStyle = style [("width", "250px"), ("display", "inline-block"), ("margin-left", "5px")]
 
 svgStyle : Html.Attribute Msg
-svgStyle = style [("width", "500px")]
+svgStyle = style [ ("width", "450px"), ("height", "150px"), ("background-color", "#FAFAFA"), ("margin", "5px") ]
 
 f : List ( List Float ) -> Int
 f x = ( List.sum << ( List.map (\a -> 1 ) ) ) x
+
+h: Polygon -> String
+h x =
+  (   String.concat
+  <<      List.map ( String.join " " ) 
+  <<  (   List.map << List.map  ) ( String.join "," )
+  <<  ( ( List.map << List.map << List.map ) (\t -> toString t) )
+  ) x.coordinates
+
+drawTiles : Maybe Polygon -> Svg Msg
+drawTiles x  = case x of
+  Nothing -> polygon [] []
+  Just y  -> polygon [ points <| h y ] []
 
 view model =
   div [ ]
     [ div [ divStyle ] [ text model.note ]
     , div [ divStyle ] [ button [ onClick AskMap ] [ text "get Map" ] ]
-    , div [ svgStyle ] [ svg [] []  ]
+    , div [ ] [ svg [ svgStyle ] [ g [] <| List.map drawTiles model.polygons ]  ]
     , div []
         <| List.map
             (\x ->
               case x of
-                Just y -> div []
+                Just y -> div [ divStyle ]
                   [ text "# elements: "
                   , ( text << toString << List.sum << ( List.map f ) ) y.coordinates
                   ]
-                Nothing -> div [] [ text "nothing"]
+                Nothing -> div [ divStyle ] [ text "nothing"]
             ) model.polygons
     ]
 
@@ -80,9 +106,14 @@ getPRData =
 
 -- no monads!!
 decodePR : Decoder  ( List ( Maybe Polygon ))
-decodePR = ( ( at ["municipios"] ) << list << maybe ) ( polygonDecoder )
+decodePR = ( ( at ["municipios"] ) << list << maybe ) ( oneOf  [ polygonDecoder ] )
 
 polygonDecoder : Decoder Polygon
-polygonDecoder = map Polygon
-  <|  at ["geometry", "coordinates"]
-  <|  ( list << list << list ) ( float )
+polygonDecoder = map2 Polygon
+  ( at [ "properties", "NAME" ] string )
+  ( at [ "geometry", "coordinates" ] <| ( list << list << list ) ( float ) )
+
+multiPolygonDecoder : Decoder MultiPolygon
+multiPolygonDecoder = map2 MultiPolygon
+  ( at [ "properties", "NAME" ] string )
+  ( at [ "geometry", "coordinates" ] <| ( list << list << list << list ) ( float ) )
