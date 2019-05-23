7d7038e3412cdd59496d874c6ce2c32c379750e6
JM <john.mangual@gmail.com>
2019-05-23 13:27:37 -0400

    fail

---
+++ arraylist.elm
@@ -0,0 +1,9 @@
+$ elm repl
+---- Elm 0.19.0 ----------------------------------------------------------------
+Read <https://elm-lang.org/0.19.0/repl> to learn more: exit, help, imports, etc.
+--------------------------------------------------------------------------------
+
+> import Array
+> Array.initialize 4 (\a -> [])  
+Array.fromList [[],[],[],[]] : Array.Array (List a)
+-- fail
